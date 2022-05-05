044dc682d1537f0c3e47941c2cb08c1ab9c8337c
Devin <devingarcia5423@gmail.com>
2022-05-05 15:40:54 -0400

    commit tree 5ba38b5a70367fa14d18eb0419ba949909de1933

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    } else {
+        dodger.style.left = "180px";
+        }
+
+
+}
+
+function moveDodgerRight () {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    } else {
+          dodger.style.left = "180px";
+      }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    };
+});
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    };
+});
+
+// document.addEventListener("keydown", function (e) {
+//     if (e.key === "ArrowRight") {
+//         moveDodgerRight();
+//         alert('I was clicked!');
+//     };
+// });
